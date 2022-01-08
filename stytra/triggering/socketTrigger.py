24cc30d46e0a1e73aa46d4bc566102b555722a3b
Matteo Dommanget-Kott <matteo.d.kott@gmail.com>
2022-01-09 00:12:06 +0100

    filling docstrings

--- stytra/triggering/socketTrigger.py
+++ stytra/triggering/socketTrigger.py
@@ -1,22 +1,23 @@
-import socket
 import json
-import time
+import socket
 import tempfile
+import time
 
 from stytra.triggering import Trigger
+
 
 class SocketTrigger(Trigger):
     """This trigger uses vanilla python sockets.
 
-        It receives a json file from the Matlab LightSheet programm upon run start.
+    It receives a json file from the Matlab LightSheet programm upon run start.
     """
 
-    def __init__(self,port=5555):
+    def __init__(self, port=5555):
         """Parameters.
 
         :port: <int or 'auto'> port on which communication will happen.
-                            if int : use this in as port number
-                            if 'auto' : finds available port and saves to temp file
+                        if int : use this in as port number
+                        if 'auto' : finds available port and saves to temp file
         """
         self.port = port
         self.protocol_duration = None
@@ -24,47 +25,47 @@ class SocketTrigger(Trigger):
         super().__init__()
 
     def check_trigger(self):
+        """Check if trigger signal was received."""
         try:
             datab = self.sock.recv(1000)
         except socket.error as error:
             return False
 
-        stringdata = datab.decode('utf-8')
+        stringdata = datab.decode("utf-8")
         jsondata = json.loads(stringdata)
-        jsondata['TimeReceived'] = time.time()
+        jsondata["TimeReceived"] = time.time()
         self.device_params_queue.put(jsondata)
         return True
 
     def run(self):
+        """Open socket.
+
+        Opens a socket, saves the port used to a temporary file, and waits for
+        client connection.
+        """
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         if type(self.port) is int:
-            s.bind(('127.0.0.1',self.port))
-        elif self.port == 'auto':
-            s.bind(('127.0.0.1',0))
+            s.bind(("127.0.0.1", self.port))
+        elif self.port == "auto":
+            s.bind(("127.0.0.1", 0))
             self.port = s.getsockname()[1]
 
         temp = tempfile.NamedTemporaryFile(
-                    suffix='.txt', 
-                    prefix='stytra_socket_trigger_', 
-                    mode='w+t'
-                )
-        temp.writelines([f'{self.port}\n'])
+            suffix=".txt", prefix="stytra_socket_trigger_", mode="w+t"
+        )
+        temp.writelines([f"{self.port}\n"])
         temp.seek(0)
-        
+
         s.listen(5)
-        print(f'[SocketTrigger] Waiting for connection on port {self.port} ...')
+        print(f"[SocketTrigger] Waiting for connection on port {self.port} ..")
         self.sock, addr = s.accept()
         self.sock.setblocking(False)
-        print(f'[SocketTrigger] Connection received from {addr} .')
+        print(f"[SocketTrigger] Connection received from {addr} .")
 
         super().run()
 
     def complete(self):
+        """Clean up socket upon exit."""
         print("[SocketTrigger] Compete function called.")
         self.sock.close()
-
-
-
-
-
