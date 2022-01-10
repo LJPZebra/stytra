465ac8ff4a9390442d20c89065427f7c2e3e5e2a
EmeEmu <matteo.d.kott@gmail.com>
2022-01-10 15:54:28 +0100

    testing stimulations

--- stytra/examples/ljp_dev_exp.py
+++ stytra/examples/ljp_dev_exp.py
@@ -1,16 +1,48 @@
 from stytra import Stytra, Protocol
 from stytra.stimulation.stimuli.visual import Pause, FullFieldVisualStimulus
+from stytra.stimulation.stimuli import MovingGratingStimulus
+from lightparam import Param
 
 from stytra.triggering.socketTrigger import SocketTrigger
 
 
-class Nostim(Protocol):
-    name = "empty_protocol"
+import pandas as pd
+import numpy as np
+
+
+class TestProtocol(Protocol):
+    name = "test_protocol"
 
     stytra_config = dict(camera=dict(type="doublespinnaker"))
     
+    def __init__(self):
+        super().__init__()
+        # Here we define these attributes as Param s. This will automatically
+        #  build a control for them and make them modifiable live from the
+        # interface.
+        self.wait_duration = Param(10.0, limits=(0.2, None))
+        self.flash_duration = Param(1.0, limits=(0.0, None))
+        self.t_pre = Param(5.0)  # time of still gratings before they move
+        self.t_move = Param(5.0)  # time of gratings movement
+        self.grating_vel = Param(-10.0)  # gratings velocity
+        self.grating_period = Param(10)  # grating spatial period
+        self.grating_angle_deg = Param(90.0)  # grating orientation
+    
     def get_stim_sequence(self):
-        return [Pause(duration=10)]  # protocol does not do anything 
+        t = [0,self.t_pre,self.t_pre,self.t_pre + self.t_move,self.t_pre + self.t_move,2 * self.t_pre + self.t_move]
+        vel = [0, 0, self.grating_vel, self.grating_vel, 0, 0]
+        df = pd.DataFrame(dict(t=t, vel_x=vel))
+            
+        stimuli = [
+            Pause(duration=self.wait_duration),
+            FullFieldVisualStimulus(duration=self.flash_duration, color=(255, 255, 255)),
+            MovingGratingStimulus(
+                df_param=df,
+                grating_angle=self.grating_angle_deg * np.pi / 180,
+                grating_period=self.grating_period,
+            )
+        ]
+        return stimuli
     
 class FlashProtocol(Protocol):
     name = "flash_protocol"
@@ -28,4 +60,4 @@ if __name__ == "__main__":
     #trigger = SocketTrigger(port='auto')
     #s = Stytra(protocol=FlashProtocol(), scope_triggering=trigger)
     
-    s = Stytra(protocol=Nostim())
+    s = Stytra(protocol=TestProtocol())
